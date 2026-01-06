68e0c86e8d6d821bd4bfe7fd95dfaf6f386280af
Parvashukla26 <parvashukla26@gmail.com>
2026-01-07 01:33:26 +0530

    Resolved the og image bug

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,49 +13,20 @@ const poppins = Poppins({
 export const metadata = {
   title: `${siteConfig.name} - ${siteConfig.tagline}`,
   description: siteConfig.description,
-  keywords: defaultKeywords,
-  authors: [{ name: siteConfig.author }],
-  metadataBase: new URL(siteConfig.domain),
-  alternates: {
-    canonical: "/",
-  },
-  manifest: "/site.webmanifest",
-  icons: {
-    icon: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
-  },
   openGraph: {
     title: `${siteConfig.name} - ${siteConfig.tagline}`,
     description: siteConfig.description,
-    url: siteConfig.domain,
-    siteName: siteConfig.name,
     images: [
       {
-        url: `${siteConfig.domain}/og-v1.png`,
+        url: "/og.png",
         width: 1200,
         height: 630,
       },
     ],
-    locale: siteConfig.locale,
-    type: "website",
   },
   twitter: {
     card: "summary_large_image",
-    title: `${siteConfig.name} - ${siteConfig.tagline}`,
-    description: siteConfig.description,
-    images: [`${siteConfig.domain}/og-v1.png`],
-    creator: siteConfig.twitterHandle,
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
+    images: ["/og.png"],
   },
 };
 
